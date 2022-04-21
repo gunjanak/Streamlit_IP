3050643f7f5ac5659dda21d72a1261444167e723
gunjanak <janak.klal@gmail.com>
2022-04-21 19:57:10 +0545

    just change title

--- image_hw2.py
+++ image_hw2.py
@@ -25,7 +25,7 @@ def main():
     image = load_image('cover.png')
     #image = Image.fromarray(image)
     st.title('075 MSICE 009')
-    st.title('Digital Image Processing Project Work')
+    st.title('Digital Image Processing Project')
     st.sidebar.title('Sidebar')
 
     menu = ['None','Basic','Spatial','Derivative','Frequency','Morphology','Logical']
